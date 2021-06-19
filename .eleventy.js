98b5faccf5451d38b45041953d14a3706cc7aec7
kimbergee <kimber.gee@gmail.com>
2021-06-19 16:20:58 -0400

    clean up styles and other stuff and update npm scripts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src/scss/");
-  eleventyConfig.addPassthroughCopy("src/css");
+  // eleventyConfig.addPassthroughCopy("src/css/base.css");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/js");
